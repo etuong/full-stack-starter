4c33762faa442168e2e4e8599c65b9ac60b8f383
etuong <etuong@gmail.com>
2024-03-09 12:52:03 -0800

    Sanitize

--- backend/index.js
+++ backend/index.js
@@ -1,65 +1,38 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
+const mysql = require("mysql2/promise");
+
 app.use(express.json());
-var cors = require('cors')
 app.use(cors())
 
+require('dotenv').config()
+console.log(process.env)
 const config = {
   db: {
-    host: "sql3.freemysqlhosting.net",
-    user: "sql3688793",
-    password: "h2B8llCsvf",
-    database: "sql3688793"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   }
 }
 
-const mysql = require("mysql2/promise");
 async function query(sql) {
   const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.query(sql);
+  const [results,] = await connection.query(sql);
   return results;
 }
 
-app.get("/students", async (req,res)=> {
+app.get("/students", async (req, res) => {
   const students = await query("select * from Roster")
   res.json(students);
 })
 
 app.post("/new-student", async (req, res) => {
-  const {name, grade} = req.body;
+  const { name, grade } = req.body;
   const sql = `insert into Roster(name, grade) values("${name}", "${grade}")`;
   const students = await query(sql)
   res.json(students);
 })
 
-const users = [
-  {
-    "name": "Ethan",
-    "email": "etuong@uw.edu",
-    "number": "123-456-7890",
-  },
-  {
-    "name": "Happy",
-    "email": "happy@uw.edu",
-    "number": "987-744-1234",
-  }
-]
-app.get("/", (req, res) => {
-  res.send("Ethan is such a nice professor");
-})
-
-app.get("/users", (req, res) => {
-  res.json(users);
-})
-
-app.get("/users/:id", (req, res) => {
-  res.json(users[Number(req.params.id) - 1]);
-})
-
-app.post("/whatever", (req, res) => {
-  users.push(req.body);
-  res.json(users);
-})
-
 app.listen(1234);
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,18 @@ function App() {
   const [students, setStudents] = React.useState([]);
 
   React.useEffect(() => {
-    fetch("https://roster-backend-m4bn.onrender.com/students")
+    // Change this to your backend server REST endpoint e.g localhost
+    const url = "https://roster-backend-m4bn.onrender.com/students";
+    fetch(url)
       .then(res => res.json())
       .then(data => {
         setStudents(data)
-        console.log(data);
       })
   }, [])
 
   return (
     <div className="App">
-      <p>EThan</p>
+      <p>Roster</p>
       {
         students && students.map((student, index) =>
           <p key={index}>{student.name} has grade {student.grade}</p>
